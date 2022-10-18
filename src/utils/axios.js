4b8af85cc4189d052eaae286b1cf068d7b5a540d
Atav <atav.daniyalov01@gmail.com>
2022-10-18 14:57:21 +0300

    add socket url to .env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { useAuthStore } from "../stores/login"
+import axios from "axios";
+import { useAuthStore } from "../stores/login";
 
 const loginConfig = {
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -7,29 +7,29 @@ const loginConfig = {
     "Content-Type": "application/json",
   },
   timeout: 5000,
-}
+};
 
-export const DefaultAPIInstance = axios.create(loginConfig)
+export const DefaultAPIInstance = axios.create(loginConfig);
 
 DefaultAPIInstance.interceptors.request.use(
   async (config) => {
-    const authStore = useAuthStore()
-    config.headers.Authorization = authStore.token
-    return config
+    const authStore = useAuthStore();
+    config.headers.Authorization = authStore.token;
+    return config;
   },
   (error) => Promise.reject(error)
-)
+);
 
 DefaultAPIInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-   const errorCode = error.response.data.code
+    const errorCode = error.response.data.code;
     if (errorCode === 44 || errorCode === 45) {
-      const authStore = useAuthStore()
-      authStore.token = ''
-      authStore.logout()
-      return axios(error.config)
+      const authStore = useAuthStore();
+      authStore.token = "";
+      authStore.logout();
+      return axios(error.config);
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
--- src/utils/socketio.js
+++ src/utils/socketio.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const soketInstance = io("http://tm.ellco.ru:6000", {
+export const soketInstance = io(import.meta.env.VITE_SOCKET_URL, {
   //   transports: ["websocket"],
   query: {
     authorization: localStorage.getItem("token"),
